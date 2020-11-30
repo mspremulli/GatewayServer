0800c92eccc5c091c68851770ad20281b1f1eb54
Michael Spremulli <michaels.wpi@gmail.com>
2020-11-30 11:36:09 -0500

    moved Users to separate Repository.  Added server switch

--- Financial Microservice/src/main/java/com/gateway/Gateway.java
+++ Financial Microservice/src/main/java/com/gateway/Gateway.java
@@ -4,18 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+
 @SpringBootApplication
 @RestController
 public class Gateway {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Gateway.class, args);
+  private static HashMap<String, Integer> servers;
+
+  public static void initializeServers(){
+    servers = new HashMap<>();
+    servers.put("LOGIN", 8000);
+    servers.put("SQLDB", 7000);
+    servers.put("ML", 6000);
   }
 
-
+  public static void main(String[] args) {
+    SpringApplication.run(Gateway.class, args);
+    initializeServers();
+  }
 
   @GetMapping
   public String sendRequest(@RequestParam String jsonRequest, String destination){
+    int serverAddress = servers.get(destination);
+
     return jsonRequest;
   }
 
