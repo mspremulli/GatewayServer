ec2b0ec74b0e60f444b9a9e6afa51ad10b4b635c
Michael Spremulli <michaels.wpi@gmail.com>
2020-12-03 09:47:18 -0500

    use HTTPClient to send request

--- Financial Microservice/src/main/java/com/gateway/GatewayApplication.java
+++ Financial Microservice/src/main/java/com/gateway/GatewayApplication.java
@@ -2,27 +2,41 @@ package com.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
+//import org.json.*;
 
 @SpringBootApplication
 public class GatewayApplication {
+  private static HashMap<String, String> servers;
+  private HttpClient serverClient = HttpClient.newBuilder().build();
+  private HttpRequest serverRequest;
 
   public static void main(String[] args) {
     SpringApplication.run(GatewayApplication.class, args);
     initializeServers();
   }
 
-  private static HashMap<String, Integer> servers;
-  private final HttpClient serverConnection = HttpClient.newBuilder().build();
-
-
   public static void initializeServers(){
     servers = new HashMap<>();
-    servers.put("LOGIN", 8000);
-//    servers.put("SQL", 7000);
-//    servers.put("ML", 6000);
+    servers.put("LOGIN", "http://localhost:8000/");
+    servers.put("SQL", "http://localhost:7000/");
+    servers.put("ML", "http://localhost:6000/");
+    servers.put("TEST", "https://jsonmock.hackerrank.com/api/movies/search/?Title=spiderman");
+  }
+
+  public String switchRoute(@RequestBody String request, @RequestParam String location){
+    String url = servers.get(location);
+    serverRequest = HttpRequest.newBuilder().uri(URI.create(url)).build();
+    serverClient.sendAsync(serverRequest, HttpResponse.BodyHandlers.ofString());
+
+    return serverRequest.toString();
   }
 
 }
