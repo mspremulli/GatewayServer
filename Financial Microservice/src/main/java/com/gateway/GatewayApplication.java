0f2e814be76b2fcd5a7a65e618719c16290423da
Michael Spremulli <michaels.wpi@gmail.com>
2020-12-03 15:42:38 -0500

    added Delete and Put routes

--- Financial Microservice/src/main/java/com/gateway/GatewayApplication.java
+++ Financial Microservice/src/main/java/com/gateway/GatewayApplication.java
@@ -63,36 +63,36 @@ public class GatewayApplication {
     return response.body();
   }
 
-//  @PutMapping("/{location}")
-//  public Object putRoute(@RequestBody String request, @PathVariable String location) throws IOException, InterruptedException {
-//    String url = servers.get(location) + request;
-//    HttpRequest.BodyPublisher body = null;
-//
-//    HttpClient serverClient = HttpClient.newHttpClient();
-//    HttpRequest serverRequest = HttpRequest.newBuilder()
-//            .PUT(body)
-//            .header("Content-Type", "application/json")
-//            .uri(URI.create(url))
-//            .build();
-//    HttpResponse<String> response = serverClient.send(serverRequest, HttpResponse.BodyHandlers.ofString());
-//
-//    return response.body();
-//  }
-//
-//  @DeleteMapping("/{location}")
-//  public Object deleteRoute(@RequestBody String request, @PathVariable String location) throws IOException, InterruptedException {
-//    String url = servers.get(location) + request;
-//
-//    HttpClient serverClient = HttpClient.newHttpClient();
-//    HttpRequest serverRequest = HttpRequest.newBuilder()
-//            .DELETE()
-//            .header("Content-Type", "application/json")
-//            .uri(URI.create(url))
-//            .build();
-//    HttpResponse<String> response = serverClient.send(serverRequest, HttpResponse.BodyHandlers.ofString());
-//
-//    return response.body();
-//  }
+  @PutMapping("/{location}")
+  public Object putRoute(@RequestBody String request, @PathVariable String location) throws IOException, InterruptedException {
+    String url = servers.get(location) + request;
+    HttpRequest.BodyPublisher body =  HttpRequest.BodyPublishers.ofFile(Paths.get(request));
+
+    HttpClient serverClient = HttpClient.newHttpClient();
+    HttpRequest serverRequest = HttpRequest.newBuilder()
+            .PUT(body)
+            .header("Content-Type", "application/json")
+            .uri(URI.create(url))
+            .build();
+    HttpResponse<String> response = serverClient.send(serverRequest, HttpResponse.BodyHandlers.ofString());
+
+    return response.body();
+  }
+
+  @DeleteMapping("/{location}")
+  public Object deleteRoute(@RequestBody String request, @PathVariable String location) throws IOException, InterruptedException {
+    String url = servers.get(location) + request;
+
+    HttpClient serverClient = HttpClient.newHttpClient();
+    HttpRequest serverRequest = HttpRequest.newBuilder()
+            .DELETE()
+            .header("Content-Type", "application/json")
+            .uri(URI.create(url))
+            .build();
+    HttpResponse<String> response = serverClient.send(serverRequest, HttpResponse.BodyHandlers.ofString());
+
+    return response.body();
+  }
 }
 
 
