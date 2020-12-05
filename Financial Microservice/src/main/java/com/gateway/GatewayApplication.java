7e2bba6dfa4221cce5e609faa52148eae571e385
Michael Spremulli <michaels.wpi@gmail.com>
2020-12-05 09:20:44 -0500

    switched to Path variables

--- Financial Microservice/src/main/java/com/gateway/GatewayApplication.java
+++ Financial Microservice/src/main/java/com/gateway/GatewayApplication.java
@@ -11,7 +11,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Paths;
 import java.util.HashMap;
-//import org.json.*;
 
 @SpringBootApplication
 @RestController
